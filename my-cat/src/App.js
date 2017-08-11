5da0bff0b47f3325d2e6ddc125d0fa0c552bd7d9
Steph Dietzel <stephdietzel@users.noreply.github.com>
2017-08-11 10:27:31 -0700

    Add some cats

--- my-cat/src/App.js
+++ my-cat/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import cat from './img/SetState.png';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={cat} className="App-logo" alt="logo" />
+          <h2>Welcome to React Cats</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
